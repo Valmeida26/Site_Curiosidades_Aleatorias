165423392bede3b3c04d16311e9fb846a1b94495
Valmeida26 <93141751+Valmeida26@users.noreply.github.com>
2022-03-11 12:40:32 -0300

    Add files via upload

---
+++ site_forum/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+from forum import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', views.home),
+    # path('posts/<int:post_id>', views.post),
+    path('', include('forum.urls', namespace='forum'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
